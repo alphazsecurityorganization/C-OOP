4eb2e13c4648ba3fd7e4e59a968c6de3a7652625
Alpha Z Security Organization <alphazsec@gmail.com>
2025-10-08 01:47:52 -0700

    Create 6 (Properties).cs

---
+++ 6 (Properties).cs
@@ -0,0 +1,66 @@
+class Person
+{
+  private string name; // field
+
+  public string Name   // property
+  {
+    get { return name; }   // get method
+    set { name = value; }  // set method
+  }
+}
+
+
+
+
+
+
+
+
+
+
+class Person
+{
+  private string name; // field
+  public string Name   // property
+  {
+    get { return name; }
+    set { name = value; }
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Person myObj = new Person();
+    myObj.Name = "Liam";
+    Console.WriteLine(myObj.Name);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class Person
+{
+  public string Name  // property
+  { get; set; }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Person myObj = new Person();
+    myObj.Name = "Liam";
+    Console.WriteLine(myObj.Name);
+  }
+}
